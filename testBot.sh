186e16da05560f99d157dbca0aad26ed54fca501
Mark Jannenga <jannengm@mail.gvsu.edu>
2017-01-26 17:41:01 -0500

    Multithreaded testing script

--- testBot.sh
+++ testBot.sh
@@ -4,7 +4,7 @@
 boardSize="30 30"
 
 #Number of games per bot:
-numTests=1
+numTests=10
 
 #Path to MyBot:
 myPath="MyBot.py"
@@ -18,7 +18,9 @@ numBots=$(ls $testPath/*Bot.py | wc -l)
 printf "%15s%15s%15s\n" "Bot Name" "Win Percent" "Average Moves" > report
 echo "=============================================" >> report
 
+#Initialize loop variables
 testsRun=0
+
 printf "\rRunning Tests...%d%%" "$((100*$testsRun/($numBots*$numTests)))"
 for Bot in $testPath/*Bot.py
 do
@@ -26,44 +28,46 @@ do
 	numWins=0
 	sumMoves=0
 
+	#Get Bot name
+	s=${Bot#*/}
+	botName=${s%%.*}
+
 	while [ "$n" -lt "$numTests" ]; do
-		#Run test
-		./halite -q -d "$boardSize" "python3 $myPath" "python3 $Bot" > temp
-
-		#Read in results
-		line=$(cat temp | grep -e '^1 ?*')
-		arr=($line)
-
-		#Get Bot name
-		s=${Bot#*/}
-		botName=${s%%.*}
-
-		#Print win or loss, collect results
-		if [ "${arr[1]}" -eq "1" ]; then
-			# echo "won against $botName"
-			((numWins+=1))
-		# else
-			# echo "lost against $botName"
-		fi
-		((sumMoves+=${arr[2]}))
+		./halite -q -d "$boardSize" "python3 $myPath" "python3 $Bot" > "temp$n" &
 
 		#Increment loop counter
 		((n+=1))
 		((testsRun+=1))
-		printf "\rRunning Tests...%d%%" "$((100*$testsRun/($numBots*$numTests)))"
+	done
+
+	#Wait for background processes to finish
+	wait
+
+	printf "\rRunning Tests...%d%%" "$((100*$testsRun/($numBots*$numTests)))"
+
+	#Collate results
+	for result in temp*
+	do
+		#Read in results
+		line=$(cat $result | grep -e '^1 ?*')
+		arr=($line)
+
+		#Print win or loss, collect results
+		if [ "${arr[1]}" -eq "1" ]; then
+			((numWins+=1))
+		fi
+		((sumMoves+=${arr[2]}))
 	done
 
 	#Calculate win percent and average moves
 	let winPercent=numWins*100/numTests
 	let avgMoves=sumMoves/numTests
-	# echo "Win percent: $winPercent%"
-	# echo "Average moves: $avgMoves"
 	printf "%15s%14d%%%15d\n" "$botName" "$winPercent" "$avgMoves" >> report
 done
 
 printf "\n"
 
-rm temp
+rm temp*
 rm *.hlt
 
 cat report
