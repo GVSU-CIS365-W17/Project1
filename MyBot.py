ce066bbadf344aae7700b885f6a470ffca071da4
Mark Jannenga <jannengm@mail.gvsu.edu>
2017-01-27 19:53:43 -0500

    Added heuristic tiebreaking to border search. Minimal improvement

--- MyBot.py
+++ MyBot.py
@@ -37,65 +37,112 @@ def check_strong_enough(square):
     return False
 
 
-# Finds the direction of the closest border
+# Finds the direction of the closest border breaks ties based
+# based on heuristic sum scores
 def find_nearest_enemy_direction(square):
     direction = NORTH
-    maxDistance = min(game_map.width, game_map.height) / 2
+    max_distance = min(game_map.width, game_map.height) / 2
+    max_heuristic = 0
 
     for d in (NORTH, EAST, SOUTH, WEST):
         distance = 0
         neighbor = game_map.get_target(square, d)
-        while neighbor.owner == myID and distance < maxDistance:
+        while neighbor.owner == myID and distance < max_distance:
             distance += 1
             neighbor = game_map.get_target(neighbor, d)
 
-        if distance < maxDistance:
+        score = sum_heuristic_neighbors(neighbor)
+        if distance < max_distance:
             direction = d
-            maxDistance = distance
+            max_distance = distance
+            max_heuristic = score
+
+        elif distance == max_distance and score > max_heuristic:
+            direction = d
+            max_heuristic = score
 
     return direction
 
 
-#Broken and time out
-# def find_nearest_border(square):
-#     direction = NORTH
-#     max_distance = min(game_map.width, game_map.height) / 2
-#     nearest_border = border_list[0]
-#
-#     #Find closest border
-#     for border_square in border_list:
-#         distance = game_map.get_distance(square, border_square)
-#         if distance < max_distance:
-#             max_distance = distance
-#             nearest_border = border_square
-#
-#     # Find shortest path to closest border
-#     for d in (NORTH, EAST, SOUTH, WEST):
-#         neighbor = game_map.get_target(square, d)
-#         distance = game_map.get_distance(neighbor, nearest_border)
-#         if distance < max_distance:
-#             max_distance = distance
-#             direction = d
-#
-#     return direction
+# Finds the direction of the closest border breaks ties based
+# based on heuristic sum scores
+def find_heuristic_enemy_direction(square):
+    direction = NORTH
+    max_distance = min(game_map.width, game_map.height) / 2
+    max_heuristic = 0
+
+    for d in (NORTH, EAST, SOUTH, WEST):
+        distance = 0
+        neighbor = game_map.get_target(square, d)
+        while neighbor.owner == myID and distance < max_distance:
+            distance += 1
+            neighbor = game_map.get_target(neighbor, d)
+
+        score = sum_heuristic_neighbors(neighbor)
+        if distance < max_distance:
+            direction = d
+            max_distance = distance
+            max_heuristic = score
+
+        elif distance == max_distance and score > max_heuristic:
+            direction = d
+            max_heuristic = score
+
+    return direction
 
 
-# def find_max_heuristic_border_direction(square):
-#     direction = NORTH
-#     maxDistance = min(game_map.width, game_map.height) / 2
-#
-#     for d in (NORTH, EAST, SOUTH, WEST):
-#         distance = 0
-#         neighbor = game_map.get_target(square, d)
-#         while neighbor.owner == myID and distance < maxDistance:
-#             distance += 1
-#             neighbor = game_map.get_target(neighbor, d)
-#
-#         if distance < maxDistance:
-#             direction = d
-#             maxDistance = distance
-#
-#     return direction
+# Finds the direction to the closest border in terms of
+# Manhattan distance
+def find_nearest_border(square):
+    direction = NORTH
+    max_distance = min(game_map.width, game_map.height) / 2
+    nearest_border = border_list[0]
+
+    #Find closest border
+    for border_square in border_list:
+        distance = game_map.get_distance(square, border_square)
+        if distance < max_distance:
+            max_distance = distance
+            nearest_border = border_square
+
+    # Find shortest path to closest border
+    for d in (NORTH, EAST, SOUTH, WEST):
+        neighbor = game_map.get_target(square, d)
+        distance = game_map.get_distance(neighbor, nearest_border)
+        if distance < max_distance:
+            max_distance = distance
+            direction = d
+
+    return direction
+
+
+# Finds the direction to the closest border in terms of
+# Manhattan distance with the highest heuristic sum
+def find_max_heuristic_border_direction(square):
+    direction = NORTH
+    # max_distance = max(game_map.width, game_map.height)
+    # max_heuristic = 0
+    max_heuristic_score = 0
+    max_heuristic_border = border_list[0]
+
+    # Find border square with the highest heuristic score
+    for border_square in border_list:
+        distance = game_map.get_distance(square, border_square)
+        score = sum_heuristic_neighbors(border_square)
+        if score / distance > max_heuristic_score:
+            max_distance = distance
+            max_heuristic_score = score / distance
+            max_heuristic_border = border_square
+
+    # Find shortest path to closest border
+    for d in (NORTH, EAST, SOUTH, WEST):
+        neighbor = game_map.get_target(square, d)
+        distance = game_map.get_distance(neighbor, max_heuristic_border)
+        if distance < max_distance:
+            max_distance = distance
+            direction = d
+
+    return direction
 
 
 # Finds the neighbor with the highest overkill heuristic
@@ -156,6 +203,8 @@ def assess_move(square):
     # Move towards the closest border if not a border piece
     if not check_is_border(square):
         return Move(square, find_nearest_enemy_direction(square))
+        # return Move(square, find_nearest_border(square))
+        # return Move(square, find_max_heuristic_border_direction(square))
 
     # Otherwise wait
     return Move(square, STILL)
